5ca46d495a49028c7472a6ca8486592087d24977
jin-sung-oh <wlsqoren@naver.com>
2025-06-16 14:24:53 +0900

    feat:약간수정

--- src/main/java/back/vybz/like_service/like/application/service/LiveLikeServiceImpl.java
+++ src/main/java/back/vybz/like_service/like/application/service/LiveLikeServiceImpl.java
@@ -7,29 +7,26 @@ import back.vybz.like_service.like.domain.mongodb.LiveLike;
 import back.vybz.like_service.like.dto.request.RequestLiveLikeDto;
 import back.vybz.like_service.like.infrastructure.LiveLikeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-
-
 @Service
 @RequiredArgsConstructor
-public class LiveLikeServiceImpl implements LiveLikeService{
+public class LiveLikeServiceImpl implements LiveLikeService {
 
     private final LiveLikeRepository liveLikeRepository;
     private final LiveLikeRedisService liveLikeRedisService;
 
     @Override
     @Transactional
-    public void likeLiveStream(RequestLiveLikeDto requestLiveLikeDto){
+    public void likeLiveStream(RequestLiveLikeDto requestLiveLikeDto) {
 
         boolean isLiked = liveLikeRepository.existsByLikerUuidAndStreamKey(
                 requestLiveLikeDto.getLikerUuid(),
                 requestLiveLikeDto.getStreamKey()
         );
-        if (isLiked){
+        if (isLiked) {
             throw new BaseException(BaseResponseStatus.DUPLICATE_LIVE_LIKE);
         }
 
